65a684d18e17dace312836aa0be11570e4c13d41
Surya Prakash Mallah <sfrost488@gmail.com>
2024-05-19 02:58:43 +0530

    user login && logout route and controller added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -54,16 +54,55 @@ export const signup = async (req, res) => {
         res.status(400).json({ error : "Invalid user data" })
     }
     
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error : "Internal Server Error"})
+  }
 };
 
 
 export const login = async (req, res) => {
-  res.json({
-    data: "You hit the login endpoint",
-  });
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({username});
+    if(!user){
+      return res.status(400).json({ error : "Invalid username"})
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+    if(!isPasswordCorrect){
+      return res.status(400).json({ error : "Invalid password"})
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id : user._id,
+      fullname : user.fullname,
+      username : user.username,
+      email : user.email,
+      followers : user.followers,
+      following : user.following,
+      profileImg : user.profileImg,
+      converImg: user.coverImg
+   })
+
+
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error : "Internal Server Error"})
+  }
 };
 
 export const logout = async (req, res) => {
-  res.json({ data: "You hit the logout endpoint" });
+  try {
+    res.cookie('jwt',"",{maxAge : 0})
+    res.status(200).json({ message : "Logged out successfully "})
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error : "Internal Server Error"})
+  }
 };
+
+export const 
