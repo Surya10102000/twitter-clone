904d7955ca6693d1c712fee577e90a9cdbdc8598
Surya Prakash Mallah <sfrost488@gmail.com>
2024-05-19 16:22:09 +0530

    all auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.routes.js"
 import connectDB from "./db/connectMongoDB.js"
@@ -11,6 +12,8 @@ const PORT = process.env.PORT
 //middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended : true}))
+
+app.use(cookieParser())
 
 app.use("/api/auth",authRoutes);
 
