9fbc86a923937340757f60a5e360331e167527ec
Surya Prakash Mallah <sfrost488@gmail.com>
2024-05-23 23:22:47 +0530

    created the Homepage with post.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import SignupPage from "./pages/auth/SignupPage";
 import LoginPage from "./pages/auth/LoginPage";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/home/HomePage";
+import Sidebar from "./components/Sidebar";
+import RightPanel from "./components/RightPanel";
 
 function App() {
   return (
     <>
-      <div className="flex max-w-6xl mx-auto"></div>
+      <div className="flex max-w-6xl mx-auto">
+      <Sidebar/>  
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/signup" element={<SignupPage/>}/>
       </Routes>
+      <RightPanel/>
+      </div>
     </>
   );
 }
