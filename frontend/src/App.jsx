f23345ebda563555ddf560c1273c08ba22733264
Surya Prakash Mallah <sfrost488@gmail.com>
2024-05-24 20:40:28 +0530

    added profilePage - UI design completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import SignupPage from "./pages/auth/SignupPage";
 import LoginPage from "./pages/auth/LoginPage";
 import HomePage from "./pages/home/HomePage";
+import NotificationPage from "./pages/notification/notificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
 import Sidebar from "./components/Sidebar";
 import RightPanel from "./components/RightPanel";
 
@@ -14,6 +17,8 @@ function App() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/signup" element={<SignupPage/>}/>
+        <Route path="/notifications" element={<NotificationPage/>}/>
+        <Route path="/profile/:username" element={<ProfilePage/>}/>
       </Routes>
       <RightPanel/>
       </div>
