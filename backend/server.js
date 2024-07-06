e60248c7920ed2fd6088c06084df003a88f30545
Surya Prakash Mallah <sfrost488@gmail.com>
2024-07-06 22:00:43 +0530

    created add post function

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT
 
 //middlewares
-app.use(express.json())
+app.use(express.json({ limit : "5mb"}))
 app.use(express.urlencoded({ extended : true}))
 
 app.use(cookieParser())
